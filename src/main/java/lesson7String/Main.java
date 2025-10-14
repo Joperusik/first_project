da0e1accc9778fdf06fcbb22c883fafee6fb9778
Joperusik <volosinzena20@gmail.com>
2025-10-14 18:10:15 +0200

    lesson7

---
+++ src/main/java/lesson7String/Main.java
@@ -0,0 +1,16 @@
+package lesson7String;
+
+public class Main {
+    static void main() {
+        // String str = new String("Something"); // String str = "Something";
+
+        String str = "Hello world";
+        String str2 = ", Bruh";
+        String str3 = str + str2;
+
+
+        System.out.println(str);
+        System.out.println(str + str2);
+        System.out.println(str3);
+    }
+}
