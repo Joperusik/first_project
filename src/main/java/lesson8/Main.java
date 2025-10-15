8631699c0d97c383f3ecb86d3bae44017c05cb3c
Joperusik <volosinzena20@gmail.com>
2025-10-15 18:32:57 +0200

    lesson8

---
+++ src/main/java/lesson8/Main.java
@@ -0,0 +1,13 @@
+package lesson8;
+
+public class Main {
+    static void main() {
+        String badWord = " _|_";
+        String massage = "Hello, mfcker" + badWord + " nga!";
+
+
+        System.out.println(massage);
+        System.out.printf("Hello, mfcker %s nga!", badWord);
+
+    }
+}
