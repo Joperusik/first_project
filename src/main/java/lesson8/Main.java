9073e18f945413822f73cd76d145b74b6c2db9a2
Joperusik <volosinzena20@gmail.com>
2025-10-15 18:39:37 +0200

    lesson8.1

--- src/main/java/lesson8/Main.java
+++ src/main/java/lesson8/Main.java
@@ -9,5 +9,14 @@ public class Main {
         System.out.println(massage);
         System.out.printf("Hello, mfcker %s nga!", badWord);
 
+        for (int i = 0; i < massage.length() - 2; i++) {
+            char pos1 = massage.charAt(i);
+            char pos2 = massage.charAt(i + 1);
+            char pos3 = massage.charAt(i + 2);
+
+            if (pos1 == badWord.charAt(0) && pos2 == badWord.charAt(1) && pos3 == badWord.charAt(2)) {
+                massage = massage.replace(badWord, "###");
+            };
+        }
     }
 }
