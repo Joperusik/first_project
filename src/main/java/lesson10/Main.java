54dd349d05fd44b68364934b3a4574d4cbc828bb
Joperusik <volosinzena20@gmail.com>
2025-10-17 19:10:17 +0200

    lesson10

---
+++ src/main/java/lesson10/Main.java
@@ -0,0 +1,13 @@
+package lesson10;
+
+public class Main {
+    static void main() {
+        int a = 3;
+        if (a > 5) {
+            System.out.println("a > 5");
+        } else  {
+            System.out.println("a < 5");
+        }
+
+    }
+}
