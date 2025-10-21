a7cd199a8b2938937e5065327f2a2688d90415b3
Joperusik <volosinzena20@gmail.com>
2025-10-21 12:32:16 +0200

    homework1.1

--- src/main/java/homeworkSwitch/Main.java
+++ src/main/java/homeworkSwitch/Main.java
@@ -3,31 +3,16 @@ package homeworkSwitch;
 public class Main {
     static void main() {
 
-        int weekDays = 5;
+        int weekDays = 4;
         switch (weekDays) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("There are only seven days in a week. Bruh");
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Bruh");
         }
     }
 }
