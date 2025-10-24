f0510ece8badefbe7fe79cf1db3ad2e01d1e291a
Joperusik <volosinzena20@gmail.com>
2025-10-24 18:13:51 +0200

    lesson 15 Math, Objects

---
+++ src/main/java/lesson15MathObject/Main.java
@@ -0,0 +1,34 @@
+package lesson15MathObject;
+
+public class Main {
+    static void main() {
+
+        //стандартный клас Math
+        int max = Math.max(5, 10);
+        System.out.println("Max int: " + max);
+
+        int min = Math.min(5, 10);
+        System.out.println("Min int: " + min);
+
+        double ceiling = Math.ceil(1.5); //большую часть
+        System.out.println("CEILING: " + ceiling);
+
+        double floored = Math.floor(1.5); //меньшую часть
+        System.out.println("FLOOR: " + floored);
+
+        double rounded = Math.round(1.5); //универсальный
+        System.out.println("ROUNDED: " + rounded);
+
+        //наш способ
+        System.out.println("My method");
+
+        int min1 = MyMath.min(5, 10); //MyMath это клас
+        System.out.println("min1: " + min1);
+
+        int pow = MyMath.power(5, 3);
+        System.out.println("pow: " + pow);
+
+        double round1 = MyMath.round(4.5);
+        System.out.println("round1: " + round1);
+    }
+}
---
+++ src/main/java/lesson15MathObject/MyMath.java
@@ -0,0 +1,46 @@
+package lesson15MathObject;
+
+public class MyMath {
+
+    //min max
+    public static int min(int a, int b) {
+        if (a < b) {
+            return a;
+        } else {
+            return b;
+        }
+
+    }
+
+    public static int max(int a, int b) {
+        if (a > b) {
+            return a;
+        } else {
+            return b;
+        }
+    }
+
+    public static double abs(double a) {
+        if (a < 0) {
+            a = a * -a;
+            return a;
+        } else {
+            return a;
+        }
+    }
+
+    public static int power(int a, int b) {
+        int result = 1;
+        for (int i = 1; i <= b; i++) {
+            result *= a;
+        }
+        return result;
+    }
+
+    public static int round (double a) {
+        //4.3 -> 4
+        //4.7 -> 4
+        int result = (int) a;
+        return result;
+    }
+}
