a1ef5a602a28c27827bf812cdd05e46f7a2f0d4b
Joperusik <volosinzena20@gmail.com>
2025-11-04 13:04:33 +0100

    homework 21

--- src/main/java/homework20/Car.java
+++ src/main/java/homework20/Car.java
@@ -16,4 +16,35 @@ public class Car {
     private float fuel;
     private float consumption;
     private LocalDate dateOutOfMaintenance;
+
+    public boolean isFuelNegative() {
+        if (fuel <= 0) {
+            System.out.println("Fuel cannot be negative");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isConsumptionNegative() {
+        if (consumption <= 0) {
+            System.out.println("Consumption cannot be negative");
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isDateOutOfMaintenance(LocalDate dateOfExpire) {
+        if (LocalDate.now().isBefore(dateOfExpire) || LocalDate.now().isEqual(dateOfExpire)) {
+            System.out.println("The car is approved for driving");
+            return true;
+        } else {
+            System.out.println("The car is not allowed to be used.");
+            return false;
+        }
+    }
+
+    public void getDistance() {
+        float distance = (fuel / consumption) * 100;
+        System.out.println("The car can drive approximately " + distance + " kilometers with the current fuel level.");
+    }
 }
