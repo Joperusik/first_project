ea4ce2a75d00ea01e878003db294224ab28ab603
Joperusik <volosinzena20@gmail.com>
2025-11-28 18:25:18 +0100

    lesson 35

--- src/main/java/lesson35/Main.java
+++ src/main/java/lesson35/Main.java
@@ -1,16 +1,45 @@
 package lesson35;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
     static void main() {
         WordUtils wordUtils = new WordUtils();
+
         List<String> words = new ArrayList<>();
 
-        words.add("Yevhen");
-        wordUtils.getShortestWord(words);
+        // The Mykola ate an apple
+        words.add("The");
+        words.add("Mykola");
+        words.add("ate");
+        words.add("an");
+        words.add("apple");
+
+        String shortestWord =
+                wordUtils.getShortestWord(words);
+
+        System.out.println("Shortest word: " + shortestWord);
 
 
+        String sentence = "Mykola ate a cucumber and a tomato and a carrot ";
+
+        Set<String> uniqueWords = wordUtils.getUniqueWordsFromSentence(sentence);
+
+        System.out.println(uniqueWords);
+        // uniqueWords -> "Mykola", "ate", "a", "cucumber", "and", "tomato", "carrot" (in any order)
+
+        String sentence2 = "today was a very great day a very nice weather and I was very happy";
+        Set<String> duplicatedWords = wordUtils.getDuplicatedWordsFromSentence(sentence2);
+        System.out.println(duplicatedWords);
+
+        Set<String> mySet = new HashSet<>();
+        boolean res1 = mySet.add("Mykola");
+        boolean res2 = mySet.add("Mykola");
+        System.out.println("res1 -> " + res1);
+        System.out.println("res2 -> " + res2);
+
+        String sentence3 = "today was a very great day a very nice weather and I was very happy";
+        Map<String, Integer> wordsCount3 = wordUtils.getWordsCount(sentence3);
+        System.out.println(wordsCount3);
     }
 }
--- src/main/java/lesson35/WordUtils.java
+++ src/main/java/lesson35/WordUtils.java
@@ -1,15 +1,100 @@
 package lesson35;
 
-import java.util.List;
+import java.util.*;
 
 public class WordUtils {
 
     public String getShortestWord(List<String> words) {
-        for (int i = 0; i < words.size(); i++) {
-            String word = words.get(i);
-            System.out.println(word);
+        // words = "The", "Mykola", "ate", "an", "apple"
+        // words.size() = 5
+
+        int shortestWordLength = 999999999; // shortestWordLength = 999999999
+        String shortestWord = ""; // shortestWord = ""
+
+        for (String word : words) {
+            int wordLength = word.length();
+
+            if (wordLength < shortestWordLength) {
+                shortestWordLength = wordLength;
+                shortestWord = word;
+            }
         }
-        return "";
+
+        // shortestWord = "an"
+        return shortestWord;
     }
 
+    public Set<String> getUniqueWordsFromSentence(String sentence) {
+        // "Mykola ate a cucumber and a tomato and a carrot"
+
+
+        String[] words = sentence.split(" ");
+
+        return new HashSet<>(List.of(words));
+
+
+    }
+
+    public Set<String> getDuplicatedWordsFromSentence(String sentence) {
+        //today was a very great day and a very nice weather and I was very happy
+        String[] words = sentence.split(" ");
+
+        Set<String> uniqueWords = new HashSet<>();
+        Set<String> duplicatedWords = new HashSet<>();
+
+        for ( String word : words) {
+            boolean isNotDuplicate = uniqueWords.add(word);
+            boolean isDuplicated = !uniqueWords.add(word);
+            //result = false when word is duplicate
+            if (isNotDuplicate) {
+
+            } else {
+                duplicatedWords.add(word);
+            }
+        }
+        return duplicatedWords;
+
+
+//        Set<String> duplicatedWords = new HashSet<>();
+//
+//        for (int currentWordIndex = 0; currentWordIndex < words.length; currentWordIndex++) {
+//            String currentWord = words[currentWordIndex];
+//            //iteretion 0 = "Today"
+//            //iteration 1 = "was"
+//            //....
+//            //iteration 15 = "happy"
+//
+//            for (int anotherCurrentWordIndex = 0; anotherCurrentWordIndex < words.length; anotherCurrentWordIndex++) {
+//                String anotherCurrentWord = words[anotherCurrentWordIndex];
+//                // iteretion 0 = "Today"
+//                //iteration 1 = "was"
+//                //....
+//                //iteration 15 = "happy
+//                if(anotherCurrentWord == currentWord) {
+//                    continue;
+//                }
+//
+//                if (currentWord.equals(anotherCurrentWord)) {
+//                    //found a dupl
+//                    duplicatedWords.add(currentWord);
+//                }
+//            }
+//
+//        }
+    }
+    //K = word
+    //v = counter
+    public Map<String,Integer> getWordsCount(String sentence) {
+
+        Map<String,Integer> wordsCount = new HashMap<>();
+        String[] words = sentence.split(" ");
+
+        for (String word : words) {
+
+            wordsCount.put(word, wordsCount.getOrDefault(word, 0) + 1);
+        }
+
+
+        return  wordsCount;
+    }
 }
